127bcd2ed72cb98d0f22dd63d06e5aab7db54ec2
JasonLee <libo529@gmail.com>
2016-11-08 10:41:49 +0800

    init配置文件

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "package cas war"
+mvnw clean package -X
+
+tomcat=/home/jason/workbench/tools/servers/apache-tomcat-7.0.68
+echo "shutdown tomcat"
+sh -x $tomcat/bin/shutdown.sh
+
+echo "rm runtime cas war "
+rm -rf $tomcat/webapps/cas*
+
+echo "shudown tomcat if not shutdown"
+ps -ef|grep $tomcat|awk '{print $2}'|xargs kill 
+
+echo "cp  war to tomcat"
+cp target/cas.war $tomcat//webapps/
+
+sh -x $tomcat/bin/startup.sh
+
+tail -200f $tomcat/logs/catalina.out
