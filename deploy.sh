27679ad1a0992b9c0658697fe867355958dfec01
JasonLee <libo529@gmail.com>
2016-12-28 17:55:25 +0800

    modify seq

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+tomcat=/home/jason/workbench/tools/servers/cas-tomcat-7.0.68
+
+echo "shudown tomcat if not shutdown"
+ps -ef|grep $tomcat|awk '{print $2}'|xargs kill -9
+
 echo "package cas war"
 mvnw clean package -X
 
-tomcat=/home/jason/workbench/tools/servers/cas-tomcat-7.0.68
 echo "shutdown tomcat"
 sh -x $tomcat/bin/shutdown.sh
 
 echo "rm runtime cas war "
 rm -rf $tomcat/webapps/cas*
-
-echo "shudown tomcat if not shutdown"
-ps -ef|grep $tomcat|awk '{print $2}'|xargs kill 
 
 echo "cp  war to tomcat"
 cp target/cas.war $tomcat//webapps/
